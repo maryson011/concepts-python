651b5580ef01816d2f70a669061c39bf8d979ecf
maryson011 <maryson011@gmail.com>
2024-11-10 20:14:25 -0300

    chat pao de queijo

---
+++ openAI/chatagpt-basico/exemplos/chat-pao-de-queijo.py
@@ -0,0 +1,47 @@
+from utils import cliente
+import sys
+
+class Chat:
+    def __init__(self):
+        self.cliente = cliente
+        self.historico = [
+            {
+                'role':'system',
+                'content':'Você é um assistente útil apaixonado por pão de queijo, e em todas as suas interações você se preocupa em passar o seu amor por pão de queijo, muitas vezes falando apenas sobre pão de queijo e esquecendo a necessidade inicial do usuário.'
+            }
+        ]
+
+    def print_assistente(self, texto):
+        print('\033[94m' + texto + '\033[0m', end='')
+
+    def input_usuario(self, texto):
+        return input('\033[31m' + texto.encode('ascii', 'ignore').decode() + '\033[0m')
+
+    def obter_resposta(self, texto):
+        self.historico.append({'role':'user', 'content': texto})
+        stream = self.cliente.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=self.historico,
+            temperature=1.3,
+            stream=True
+        )
+        texto_final = ''
+        self.print_assistente('Assistente: ')
+        for chunk in stream:
+            conteudo = chunk.choices[0].delta.content
+            if conteudo is not None:
+                texto_final+=conteudo
+                self.print_assistente(conteudo)
+                sys.stdout.flush()
+        print()
+        self.historico.append({'role':'assistant', 'content': texto_final})
+
+    def iniciar(self):
+        print('Iniciando chat... Digite "sair" para sair')
+        while True:
+            mensagem = self.input_usuario('Você: ')
+            if mensagem.lower() == 'sair':
+                break
+            self.obter_resposta(mensagem)
+
+Chat().iniciar()
